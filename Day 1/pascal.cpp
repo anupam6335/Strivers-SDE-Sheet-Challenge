e09a50a947fd92b6ee80fc20b8d305e48fa26239
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-06-05 19:26:59 +0530

    code studio solution

--- Day 1/pascal.cpp
+++ Day 1/pascal.cpp
@@ -18,3 +18,27 @@ public:
         return pascal;
     }
 };
+
+//  code studio solution
+
+#include <bits/stdc++.h>
+
+vector<vector<long long int>> printPascal(int n) 
+{
+  // Write your code here.
+    vector<vector<long long int>> pascal(n);
+        
+        
+        for(int i=0; i<n; i++) {
+            pascal[i].resize(i+1);
+            
+            pascal[i][0] = pascal[i][i] = 1;
+            
+            
+            for(int j=1; j<i; j++){
+                pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j];
+            }
+        }
+        
+        return pascal;
+}
