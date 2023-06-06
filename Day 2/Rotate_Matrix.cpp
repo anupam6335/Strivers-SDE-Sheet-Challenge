b1fb8b0c584086b0a09c9dfe6dd1b1775087eb6d
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-06-06 20:14:25 +0530

    leetcode solution of rotate image or roate matrix

--- Day 2/Rotate_Matrix.cpp
+++ Day 2/Rotate_Matrix.cpp
@@ -27,3 +27,24 @@ void rotateMatrix(vector<vector<int>> &mat, int n, int m)
 
 
 // leetcode solution
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        vector<vector<int>> mat = matrix;
+        
+        int n=mat.size(), m=mat[0].size();
+        
+        int k=0,l=0;
+        
+        for(int i=n-1; i>=0; i--) {
+            for(int j=0; j<m; j++) {
+                matrix[k][l] = mat[i][j];
+                k++;
+            }
+            l++;
+            k=0;
+        }
+        
+    }
+
+};v
